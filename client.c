10717ada5e591f115c93f51c6e1fcc11d2b6765c
Driss Haouhaou <drhaouha@made-f0Cr13s4.clusters.42paris.fr>
2024-06-27 19:55:39 +0200

    minitalk github

--- client.c
+++ client.c
@@ -1,38 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
 
-#define MESSAGE_SIZE 256
+#include <limits.h>
+#include <stdint.h>
+#include <stddef.h>
+
+// #define MESSAGE_SIZE 256
 
 volatile sig_atomic_t ready_to_send = 0;
-char message[MESSAGE_SIZE];
+// char message[MESSAGE_SIZE];
 
-void handle_signal(int sig) {
-    ready_to_send = 1;
+int	ft_atoi(const char *s)
+{
+	int						sign;
+	unsigned long long int	r;
+
+	r = 0;
+	sign = 1;
+	while (*s == 32 || (*s >= 9 && *s <= 13))
+		s++;
+	if (*s == '-')
+	{
+		sign *= -1;
+		s++;
+	}
+	else if (*s == '+')
+		s++;
+	while (*s >= '0' && *s <= '9')
+	{
+		r = (r * 10) + (*s - '0');
+		s++;
+	}
+	if (r > LONG_MAX && sign < 0)
+		return (0);
+	else if (r > LONG_MAX && sign > 0)
+		return (-1);
+	return ((int)(r * sign));
 }
 
-int main() {
-    pid_t reader_pid;
-    printf("Enter the reader PID: ");
-    scanf("%d", &reader_pid);
+unsigned char reverse_bits(unsigned char octet) {
+	unsigned char octet1;
+	int	i;
 
-    // Définir le gestionnaire de signal pour SIGUSR2
-    signal(SIGUSR2, handle_signal);
+	i = -1;
+	octet1 = 0;
+	while (++i <= 7)
+		octet1 |= ((octet >> i) & 1) << (7 - i);
+	return (octet1);
+}
 
-    // Lire le message à envoyer
-    printf("Enter the message to send: ");
-    scanf("%s", message);
+void print_bits(unsigned char byte) {
+	int	i = 8;
+	unsigned char octet[i];
 
-    // Envoyer le signal au processus lecteur
-    kill(reader_pid, SIGUSR1);
+	while(--i >= 0){
+		octet[i] = (byte % 2) + '0';
+		byte /= 2;
+	}
+	write(1, octet, 8);
+}
 
-    // Attendre que le lecteur soit prêt à recevoir
-    while (!ready_to_send) {
-        pause();
-    }
+void handle_signal(int sig) {
+	ready_to_send = 1;
+}
 
-    printf("Message sent: %s\n", message);
-    return 0;
+int main(int ac, char **av) {
+	pid_t	server_pid;
+	char	*message;
+	int		kill_ret;
+
+	if (ac == 3) {
+		// printf("Enter the reader PID: ");
+		// scanf("%d", &server_pid);
+		server_pid = (pid_t)ft_atoi(av[1]);
+		message = av[2];
+
+		// Définir le gestionnaire de signal pour SIGUSR1 => (bit = 0)
+		signal(SIGUSR1, handle_signal);
+		// Définir le gestionnaire de signal pour SIGUSR2 => (bit = 1)
+		signal(SIGUSR2, handle_signal);
+
+		// Lire le message à envoyer
+		// printf("Enter the message to send: ");
+		// scanf("%s", message);
+
+		// Envoyer le signal au processus serveur
+		kill_ret = kill(server_pid, SIGUSR1);
+
+		// Attendre que le serveur soit prêt à recevoir
+		while (!ready_to_send) {
+			pause();
+		}
+
+		// printf("Message sent: %s\n", message);
+	}
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+// #include <sys/types.h>
 #include <signal.h>
 #include <unistd.h>
 
@@ -9,27 +10,31 @@ extern char message[MESSAGE_SIZE];
 volatile sig_atomic_t message_received = 0;
 
 void handle_signal(int sig) {
-    if (sig == SIGUSR1) {
-        // Afficher le message
-        printf("Received message: %s\n", message);
-        message_received = 1;
+	int		kill_ret;
 
-        // Répondre à l'écrivain
-        kill(getppid(), SIGUSR2);
-    }
+	if (sig == SIGUSR1) {
+		// Afficher le message
+		printf("Received message: %s\n", message);
+		message_received = 1;
+
+		// Répondre au client
+		kill_ret = kill(getppid(), SIGUSR2);
+	}
 }
 
 int main() {
-    // Afficher le PID pour que l'écrivain puisse l'utiliser
-    printf("Reader PID: %d\n", getpid());
+	// Afficher le PID pour que le client puisse l'utiliser
+	printf("Server PID: %d\n", getpid());
 
-    // Définir le gestionnaire de signal pour SIGUSR1
-    signal(SIGUSR1, handle_signal);
+	// Définir le gestionnaire de signal pour SIGUSR1 => (bit = 0)
+	signal(SIGUSR1, handle_signal);
+	// Définir le gestionnaire de signal pour SIGUSR2 => (bit = 1)
+	signal(SIGUSR2, handle_signal);
 
-    // Attendre que le signal arrive
-    while (!message_received) {
-        pause(); // Attendre un signal
-    }
+	// Attendre que le signal arrive
+	while (!message_received) {
+		pause(); // Attendre un signal
+	}
 
-    return 0;
+	return (0);
 }
