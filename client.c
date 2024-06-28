0e20443374312c28378fbb13d1fa9f1d09cd9f79
Driss Haouhaou <drhaouha@made-f0Dr11s5.clusters.42paris.fr>
2024-06-28 19:50:48 +0200

    minitalk github

--- client.c
+++ client.c
@@ -1,18 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <signal.h>
 #include <unistd.h>
+#include <signal.h>
 #include <string.h>
+// #include <sys/siginfo.h>
 
 #include <limits.h>
 #include <stdint.h>
 #include <stddef.h>
 
-// #define MESSAGE_SIZE 256
+// pid_t server_pid;
 
-volatile sig_atomic_t ready_to_send = 0;
-// char message[MESSAGE_SIZE];
+// Déclaration du gestionnaire de signal avec des informations supplémentaires
+// void handle_sigint(int sig, siginfo_t *siginfo, void *context) {
+//     printf("Caught signal %d (SIGINT)\n", sig);
+//     printf("Signal number: %d\n", siginfo->si_signo);
+//     printf("Sending PID: %d\n", siginfo->si_pid);
+//     printf("User time consumed: %ld\n", siginfo->si_utime);
+//     printf("System time consumed: %ld\n", siginfo->si_stime);
+//     exit(0);
+// }
+
+void handle_error(int sig, siginfo_t *siginfo, void *context) {
+	(void)sig;
+	(void)siginfo;
+	(void)context;
+    perror("sigaction");
+    exit(EXIT_FAILURE);
+}
+
+void handle_ack(int sig) {
+	(void)sig;
+    static int ack_received = 0;
+    ack_received = 1;
+	printf("ack_received: %d\n", ack_received);
+}
+
+void sendBit(int bit, pid_t server_pid) {
+    if (bit == 0) {
+        kill(server_pid, SIGUSR1);
+    } else {
+        kill(server_pid, SIGUSR2);
+    }
+    usleep(1000); // Pause pour permettre au récepteur de traiter le signal
+}
+
+void sendTextBitByBit(const char *text, pid_t server_pid) {
+    struct sigaction sa;
+    // sa.sa_handler = handle_error;
+	sa.sa_sigaction = handle_error;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
+    sa.sa_handler = handle_ack;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGUSR2, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
+    for (size_t i = 0; i < strlen(text); ++i) {
+        char ch = text[i];
+        for (int bit = 7; bit >= 0; --bit) {
+            int bitValue = (ch >> bit) & 1;
+            sendBit(bitValue, server_pid);
+			pause();
+        }
+
+        // Attendre la confirmation de réception pour l'octet
+        // sigset_t mask, oldmask;
+        // sigemptyset(&mask);
+        // sigaddset(&mask, SIGUSR1);
+        // sigprocmask(SIG_BLOCK, &mask, &oldmask);
+
+        // int ack_received = 0;
+        // struct sigaction sa;
+        // sa.sa_handler = handle_ack;
+        // sa.sa_flags = 0;
+        // sigemptyset(&sa.sa_mask);
+        // sigaction(SIGUSR1, &sa, NULL);
+
+        // while (!ack_received) {
+        //     sigsuspend(&oldmask);
+        // }
+
+        // sigprocmask(SIG_UNBLOCK, &mask, NULL);
+    }
+}
 
 int	ft_atoi(const char *s)
 {
@@ -42,61 +121,18 @@ int	ft_atoi(const char *s)
 	return ((int)(r * sign));
 }
 
-unsigned char reverse_bits(unsigned char octet) {
-	unsigned char octet1;
-	int	i;
 
-	i = -1;
-	octet1 = 0;
-	while (++i <= 7)
-		octet1 |= ((octet >> i) & 1) << (7 - i);
-	return (octet1);
-}
+int main(int argc, char *argv[]) {
+	pid_t server_pid;
+    
+	if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_pid> <text>\n", argv[0]);
+        return 1;
+    }
 
-void print_bits(unsigned char byte) {
-	int	i = 8;
-	unsigned char octet[i];
+    server_pid = (pid_t)ft_atoi(argv[1]);
+    const char *text = argv[2];
 
-	while(--i >= 0){
-		octet[i] = (byte % 2) + '0';
-		byte /= 2;
-	}
-	write(1, octet, 8);
-}
-
-void handle_signal(int sig) {
-	ready_to_send = 1;
-}
-
-int main(int ac, char **av) {
-	pid_t	server_pid;
-	char	*message;
-	int		kill_ret;
-
-	if (ac == 3) {
-		// printf("Enter the reader PID: ");
-		// scanf("%d", &server_pid);
-		server_pid = (pid_t)ft_atoi(av[1]);
-		message = av[2];
-
-		// Définir le gestionnaire de signal pour SIGUSR1 => (bit = 0)
-		signal(SIGUSR1, handle_signal);
-		// Définir le gestionnaire de signal pour SIGUSR2 => (bit = 1)
-		signal(SIGUSR2, handle_signal);
-
-		// Lire le message à envoyer
-		// printf("Enter the message to send: ");
-		// scanf("%s", message);
-
-		// Envoyer le signal au processus serveur
-		kill_ret = kill(server_pid, SIGUSR1);
-
-		// Attendre que le serveur soit prêt à recevoir
-		while (!ready_to_send) {
-			pause();
-		}
-
-		// printf("Message sent: %s\n", message);
-	}
-	return (0);
+    sendTextBitByBit(text, server_pid);
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,40 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <sys/types.h>
-#include <signal.h>
 #include <unistd.h>
+#include <signal.h>
+// #include <sys/siginfo.h>
 
-#define MESSAGE_SIZE 256
 
-extern char message[MESSAGE_SIZE];
-volatile sig_atomic_t message_received = 0;
+// pid_t clent_pid;
 
-void handle_signal(int sig) {
-	int		kill_ret;
+// Déclaration du gestionnaire de signal avec des informations supplémentaires
+// void handle_sigint(int sig, siginfo_t *siginfo, void *context) {
+//     printf("Caught signal %d (SIGINT)\n", sig);
+//     printf("Signal number: %d\n", siginfo->si_signo);
+//     printf("Sending PID: %d\n", siginfo->si_pid);
+//     printf("User time consumed: %ld\n", siginfo->si_utime);
+//     printf("System time consumed: %ld\n", siginfo->si_stime);
+//     exit(0);
+// }
 
-	if (sig == SIGUSR1) {
-		// Afficher le message
-		printf("Received message: %s\n", message);
-		message_received = 1;
+void handle_bit(int sig, siginfo_t *siginfo, void *context) {
+	(void)context;
+    static int bitValue = -1;
+    static int bitCount = 0;
+    static char currentChar = 0;
 
-		// Répondre au client
-		kill_ret = kill(getppid(), SIGUSR2);
-	}
+	printf("received: %d\n\n", sig);
+
+    if (sig == SIGUSR1) {
+        bitValue = 0;
+    } else if (sig == SIGUSR2) {
+        bitValue = 1;
+    }
+
+    currentChar = (currentChar << 1) | bitValue;
+    bitCount++;
+
+    if (bitCount == 8) {
+        printf("%c", currentChar);
+        // fflush(stdout);
+        bitCount = 0;
+        currentChar = 0;
+        // kill(siginfo->si_pid, SIGUSR1); // Envoyer confirmation de réception
+    }
+    kill(siginfo->si_pid, SIGUSR2); // Envoyer confirmation de réception
+	pause();
 }
 
 int main() {
-	// Afficher le PID pour que le client puisse l'utiliser
-	printf("Server PID: %d\n", getpid());
+	// pid_t clent_pid;
+    struct sigaction sa1, sa2;
 
-	// Définir le gestionnaire de signal pour SIGUSR1 => (bit = 0)
-	signal(SIGUSR1, handle_signal);
-	// Définir le gestionnaire de signal pour SIGUSR2 => (bit = 1)
-	signal(SIGUSR2, handle_signal);
+    // sa1.sa_handler = handle_bit;
+    sa1.sa_sigaction = handle_bit;
+    sa1.sa_flags = 0; //SA_RESTART;
+    sigemptyset(&sa1.sa_mask);
+    if (sigaction(SIGUSR1, &sa1, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
 
-	// Attendre que le signal arrive
-	while (!message_received) {
-		pause(); // Attendre un signal
-	}
+    // sa1.sa_handler = handle_bit;
+    sa1.sa_sigaction = handle_bit;
+    sa2.sa_flags = 0; //SA_RESTART;
+    sigemptyset(&sa2.sa_mask);
+    if (sigaction(SIGUSR2, &sa2, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
 
-	return (0);
+    printf("Receiver PID: %d\n", getpid());
+    // printf("Please set the sender PID: ");
+    // scanf("%d", &clent_pid);
+
+    while (1) {
+        pause();  // Attendre un signal
+    }
+
+    return 0;
 }
